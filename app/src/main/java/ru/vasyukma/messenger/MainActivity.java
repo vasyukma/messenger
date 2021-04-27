d048cfeac8f880c2aec8b147bf77cc4d08fb2526
Maksim Vasyuk <vasyukma@gmail.com>
2021-04-27 23:53:23 +0700

    the first commit

---
+++ app/src/main/java/ru/vasyukma/messenger/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.vasyukma.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
